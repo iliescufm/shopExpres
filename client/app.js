e5c313efbea0dbb554f5e3d0c4849c03c3d74f9c
Alexandru <alex2august@gmail.com>
2017-10-27 19:09:13 +0300

    Integrated user requests page with categories from db

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', ['ngMaterial']);
 
-app.controller("ListCtrl", function($scope, $timeout, $q, $log) {
+app.controller("ListCtrl", function($scope, $timeout, $q, $log, $http) {
 
 	this.arr = [
 		{
@@ -8,18 +8,17 @@ app.controller("ListCtrl", function($scope, $timeout, $q, $log) {
 			searchText: '',
 		},
 	]
+
 	var self = this;
 
-    self.simulateQuery = false;
-    self.isDisabled    = false;
-
-    // list of `state` value/display objects
-    self.states        = loadAll();
-    self.querySearch   = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange   = searchTextChange;
-
-    self.newState = newState;
+    $http.get('/api/products/getCategories').then((res) => {
+    	self.states = res.data.map((categ)=>{
+    		return {
+    			value: categ.id,
+    			display: categ.keyword + " (" + categ.unit + ") "
+    		}
+    	})
+    });
 
     self.add_new = function() {
     	this.arr.push({
@@ -28,76 +27,36 @@ app.controller("ListCtrl", function($scope, $timeout, $q, $log) {
 		})
     }
 
-    self.cumpara = function() {
+    self.genereazaCosul = function() {
     	self.arr.map((item) => {
-    		console.log(item)
-    		item.cantiate = item.searchText.match(/\d+/)[0]
+    		delte 
+    		item.quantity = item.searchText.match(/\d+/)[0]
+    		self.states.forEach((categ) => {
+    			if(item.searchText.startsWith(categ.display))
+    				item.id = categ.value
+    		})
     	})
-    	console.log(self.arr);
+
+    	$http.post('/api/product/obtainProduct', self.arr).then((res) => {
+    		console.log(res.data);
+    	})
     }
 
     self.sterge = function(idx) {
     	self.arr.splice(idx, 1);
     }
 
-    function newState(state) {
-      alert("Sorry! You'll need to create a Constitution for " + state + " first!");
-    }
-
-    // ******************************
-    // Internal methods
-    // ******************************
-
-    /**
-     * Search for states... use $timeout to simulate
-     * remote dataservice call.
-     */
-    function querySearch (query) {
-      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
-      }
-    }
-
-    function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
+    self.querySearch = function(query) {
+		return self.states.filter((state) => {
+			return state.display.startsWith(query)
+		});
     }
 
     function selectedItemChange(item) {
       $log.info('Item changed to ' + JSON.stringify(item));
     }
 
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allStates = 'Lapte, Miere, Oua, Bere, Mere_bucata, Mere_KG, \
-      Sare, Zagar, Rosii';
-
-      return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
-    }
-
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
-
-    }
+    
 
 
 });
