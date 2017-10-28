81617f4e2eba2398486fb25d3febce2c15d6eef1
Alexandru <alex2august@gmail.com>
2017-10-28 08:02:53 +0300

    FINAL!!!!... not really but meh

--- client/pages/generatedProducts/ctrl.js
+++ client/pages/generatedProducts/ctrl.js
@@ -1,4 +1,4 @@
-app.controller('GeneratedProductsCtrl', function($scope, $rootScope, $http){
+app.controller('GeneratedProductsCtrl', function($scope, $rootScope, $http, $mdDialog){
 	function getCookie(name) {
 	  var value = "; " + document.cookie;
 	  var parts = value.split("; " + name + "=");
@@ -8,6 +8,30 @@ app.controller('GeneratedProductsCtrl', function($scope, $rootScope, $http){
 	$scope.arr = $rootScope.arr;
 
     console.log($rootScope.arr);
+
+
+    var d = $mdDialog.show({
+             template: '\
+                <div class="sk-cube-grid">\
+                  <div class="sk-cube sk-cube1"></div>\
+                  <div class="sk-cube sk-cube2"></div>\
+                  <div class="sk-cube sk-cube3"></div>\
+                  <div class="sk-cube sk-cube4"></div>\
+                  <div class="sk-cube sk-cube5"></div>\
+                  <div class="sk-cube sk-cube6"></div>\
+                  <div class="sk-cube sk-cube7"></div>\
+                  <div class="sk-cube sk-cube8"></div>\
+                  <div class="sk-cube sk-cube9"></div>\
+                </div>  \
+             ',
+             controller: function($mdDialog) {
+         		setTimeout(function() {
+			    	$mdDialog.hide();
+			    }, 500*$rootScope.results.length)
+             }
+        });
+
+
 
     $rootScope.results = Array($rootScope.arr.length);
 
@@ -75,7 +99,7 @@ app.controller('GeneratedProductsCtrl', function($scope, $rootScope, $http){
 
     });
 
-
+    
     $scope.optionToDescription = function(option) {
     	return option.times + " x " + option.name;
     }
